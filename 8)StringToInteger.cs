6e605d0496366d4fd251afe760394ba829de3cf1
ilya-nuhi <92297205+ilya-nuhi@users.noreply.github.com>
2024-01-27 18:01:44 +0300

    Create 8)StringToInteger.cs

---
+++ 8)StringToInteger.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int MyAtoi(string s) {
+        List<char> chars = new();
+        s = s.TrimStart(' ');
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (Char.IsDigit(s[i]))
+            {
+                chars.Add(s[i]);
+                continue;
+            }
+            if (i == 0 && s[0] == '-')
+                continue;
+            if (i == 0 && s[0] == '+')
+                continue;
+            break;
+        }
+        if (chars.Count == 0)
+            return 0;
+        bool cor = int.TryParse(String.Join("", chars), out int result);
+        if (cor && s[0] == '-')
+            return -result;
+        if (cor)
+            return result;
+        if (!cor && s[0] == '-')
+            return int.MinValue;
+        else
+            return int.MaxValue;
+    }
+}
